379ba6c07535fb0f47e680fa4101e3621eef7163
allikaaz <35347147+allikaaz@users.noreply.github.com>
2021-11-04 20:29:30 +0000

    update_task 6

--- Task 6.py
+++ Task 6.py
@@ -1,7 +1,5 @@
 user_numb = int(input("Enter any number of your choice"))
 if user_numb < 10:
     print("Number is smaller than 10")
-    while user_numb < 10:
-        user_numb =+1 
 else:
     print("The number is greater than 10")
