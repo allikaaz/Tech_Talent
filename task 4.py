06e50b0660faed9a65840022efc79a86191ca069
allikaaz <35347147+allikaaz@users.noreply.github.com>
2021-11-15 11:35:39 +0000

    Delete task 4.py

--- task 4.py
+++
@@ -1,7 +0,0 @@
-Mtr_bike = 2000
-print("The original cost of the motor bike is: "+ str( Mtr_bike ) + "")
-perc_lst = Mtr_bike * 10/100
-Mtr_bike-= perc_lst
-while Mtr_bike >= 1000:
-    print("The new price of the motor bike is: "+ str(int(Mtr_bike)) +"")
-    Mtr_bike-= perc_lst
